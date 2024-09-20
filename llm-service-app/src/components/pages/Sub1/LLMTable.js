89e06f21fad7b0f5c670c45a9406ff9282388c26
leesujeong <leesujeong@ubisam.com>
2024-09-20 13:24:52 +0900

    폰트 추가

--- llm-service-app/src/components/pages/Sub1/LLMTable.js
+++ llm-service-app/src/components/pages/Sub1/LLMTable.js
@@ -35,11 +35,41 @@ const LLMTable = () => {
         { id: 27, time: '2024-09-11 16:45:03', model: 'GPT-4o', result: '-', risk: '-', resultFile: '-', status: 'Saving' },
         { id: 28, time: '2024-08-22 14:47:03', model: 'Gemma:7b', result: '-', risk: '120건 / 821건', resultFile: '-', status: 'Saving' },
         { id: 29, time: '2024-09-16 16:35:03', model: 'Gemini-1.5', result: '-', risk: '-', resultFile: '-', status: 'Error' },
+        { id: 30, time: '2024-09-02 18:29:03', model: 'Gemini-1.5', result: '-', risk: '-', resultFile: '-', status: 'Saving' },
+        { id: 1, time: '2024-08-24 17:02:03', model: 'Gemma:7b', result: '-', risk: '-', resultFile: '-', status: 'Success' },
+        { id: 2, time: '2024-09-04 14:43:03', model: 'GPT-4o', result: '-', risk: '120건 / 821건', resultFile: '-', status: 'Error' },
+        { id: 3, time: '2024-09-14 07:33:03', model: 'Gemma:7b', result: '-', risk: '-', resultFile: '-', status: 'Success' },
+        { id: 4, time: '2024-09-16 07:05:03', model: 'Gemini-1.5', result: '-', risk: '-', resultFile: '-', status: 'Success' },
+        { id: 5, time: '2024-09-19 19:10:03', model: 'Gemma:7b', result: '-', risk: '-', resultFile: '-', status: 'Success' },
+        { id: 6, time: '2024-08-31 14:50:03', model: 'GPT-4o', result: '-', risk: '-', resultFile: '-', status: 'Success' },
+        { id: 7, time: '2024-08-24 15:17:03', model: 'Gemini-1.5', result: '-', risk: '-', resultFile: '-', status: 'Error' },
+        { id: 8, time: '2024-09-19 13:40:03', model: 'Gemini-1.5', result: '-', risk: '-', resultFile: '-', status: 'Running' },
+        { id: 9, time: '2024-09-06 14:58:03', model: 'Gemini-1.5', result: '-', risk: '120건 / 821건', resultFile: '-', status: 'Saving' },
+        { id: 10, time: '2024-09-16 05:30:03', model: 'Gemma:7b', result: '-', risk: '-', resultFile: '-', status: 'Running' },
+        { id: 11, time: '2024-09-17 08:53:03', model: 'GPT-4o', result: '-', risk: '120건 / 821건', resultFile: '-', status: 'Error' },
+        { id: 12, time: '2024-09-04 00:41:03', model: 'Gemma:7b', result: '-', risk: '-', resultFile: '-', status: 'Error' },
+        { id: 13, time: '2024-09-12 05:28:03', model: 'GPT-4o', result: '-', risk: '-', resultFile: '-', status: 'Saving' },
+        { id: 14, time: '2024-08-27 23:41:03', model: 'Gemini-1.5', result: '-', risk: '120건 / 821건', resultFile: '-', status: 'Saving' },
+        { id: 15, time: '2024-09-09 15:13:03', model: 'Gemma:7b', result: '-', risk: '-', resultFile: '-', status: 'Running' },
+        { id: 16, time: '2024-09-07 10:05:03', model: 'GPT-4o', result: '-', risk: '-', resultFile: '-', status: 'Running' },
+        { id: 17, time: '2024-09-04 02:38:03', model: 'Gemini-1.5', result: '-', risk: '-', resultFile: '-', status: 'Saving' },
+        { id: 18, time: '2024-09-14 07:41:03', model: 'Gemini-1.5', result: '-', risk: '-', resultFile: '-', status: 'Running' },
+        { id: 19, time: '2024-09-16 11:32:03', model: 'Gemma:7b', result: '-', risk: '-', resultFile: '-', status: 'Running' },
+        { id: 20, time: '2024-09-19 01:11:03', model: 'Gemini-1.5', result: '-', risk: '120건 / 821건', resultFile: '-', status: 'Running' },
+        { id: 21, time: '2024-09-06 17:15:03', model: 'Gemini-1.5', result: '-', risk: '-', resultFile: '-', status: 'Error' },
+        { id: 22, time: '2024-09-10 22:55:03', model: 'Gemma:7b', result: '-', risk: '-', resultFile: '-', status: 'Success' },
+        { id: 23, time: '2024-09-19 00:07:03', model: 'GPT-4o', result: '-', risk: '-', resultFile: '-', status: 'Running' },
+        { id: 24, time: '2024-08-24 21:46:03', model: 'GPT-4o', result: '-', risk: '-', resultFile: '-', status: 'Saving' },
+        { id: 25, time: '2024-08-24 18:36:03', model: 'Gemini-1.5', result: '-', risk: '-', resultFile: '-', status: 'Saving' },
+        { id: 26, time: '2024-08-29 20:53:03', model: 'Gemini-1.5', result: '-', risk: '-', resultFile: '-', status: 'Error' },
+        { id: 27, time: '2024-09-11 16:45:03', model: 'GPT-4o', result: '-', risk: '-', resultFile: '-', status: 'Saving' },
+        { id: 28, time: '2024-08-22 14:47:03', model: 'Gemma:7b', result: '-', risk: '120건 / 821건', resultFile: '-', status: 'Saving' },
+        { id: 29, time: '2024-09-16 16:35:03', model: 'Gemini-1.5', result: '-', risk: '-', resultFile: '-', status: 'Error' },
         { id: 30, time: '2024-09-02 18:29:03', model: 'Gemini-1.5', result: '-', risk: '-', resultFile: '-', status: 'Saving' }
     ];
 
     const [currentPage, setCurrentPage] = useState(1);
-  const [itemsPerPage] = useState(10);
+  const [itemsPerPage] = useState(15);
 
   // 현재 페이지에 맞는 데이터를 계산
   const indexOfLastItem = currentPage * itemsPerPage;
@@ -70,7 +100,6 @@ const LLMTable = () => {
     
       return (
         <div>
-          <h2>Paginated Table</h2>
           <table className='fixed-table'>
             <thead>
                 <tr>
@@ -95,7 +124,7 @@ const LLMTable = () => {
       </table>
 
       <Pagination
-          postsPerPage={currentPage} // 페이지 당 포스트 수
+          postsPerPage={itemsPerPage} // 페이지 당 포스트 수
           totalPosts={data.length} // 전체 포스트 수
           paginate={(pageNumber) => setCurrentPage(pageNumber)} // 페이지 번호를 변경하는 함수
           currentPage={currentPage} // 현재 페이지 번호
