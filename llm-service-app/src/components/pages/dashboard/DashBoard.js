8f91148485d7661de2ef63cf2f67d7d6f3e891bf
ParkDongJun <yuki@ubisam.com>
2024-10-08 17:15:05 +0900

    머지 완료

--- llm-service-app/src/components/pages/dashboard/DashBoard.js
+++ llm-service-app/src/components/pages/dashboard/DashBoard.js
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React, { useEffect,useState } from 'react';
 import LLMTable from './LLMTable';
 import './DashBoard.css'
 import axios from 'axios';
