fba4d171a270a21746041075d5ca91283475e875
ParkDongJun <yuki@ubisam.com>
2024-10-10 12:59:11 +0900

    header 부분 수정완료

--- llm-service-app/src/components/Layouts/Header.js
+++ llm-service-app/src/components/Layouts/Header.js
@@ -19,22 +19,58 @@ function Header({ onLogout }) {
         <img alt='LG LOGO' src={`${isDarkMode ? darkLogo : lightLogo}`}  className={`logo ${isDarkMode ? 'dark-mode' : 'light-mode'}`} />
       </div>
       <div className="user-section">
-        {
-          user ? (
-            <span className={`user-name ${isDarkMode ? 'dark-mode' : ''}`}>{user.name}</span>
-          ) : (
-            <span className={`user-name ${isDarkMode ? 'dark-mode' : ''}`}>UbiSam</span>
-          )
-        } 
-         {/* 로그아웃 버튼에 onClick 이벤트 추가 */}
-         <button
+       
+        {/* <label className="userinfo">
+      
+          <svg xmlns="http://www.w3.org/2000/svg" className="icon-image" fill="none" viewBox="0 0 24 24" stroke="currentColor">
+            <path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M5.121 17.804A7.962 7.962 0 0012 20a7.962 7.962 0 006.879-2.196M15 10a3 3 0 11-6 0 3 3 0 016 0zM4 15a8 8 0 1116 0v1a4 4 0 01-4 4H8a4 4 0 01-4-4v-1z" />
+          </svg>
+
+           
+        
+          <span className="userinfo-text">UbiSam</span>
+        </label> */}
+        {/* 로그아웃 버튼에 onClick 이벤트 추가 */}
+        {/* <button
           className={`logout-button ${isDarkMode ? 'dark-mode' : ''}`}
           onClick={onLogout}
         >
           로그아웃
-        </button>
-        <button onClick={toggleTheme} style={{ marginLeft: '10px' }}>
-          {isDarkMode ? '라이트 모드' : '다크 모드'}
+        </button> */}
+        <label className="Huserinfo">
+          {/* 반쪽 사람 아이콘 */}
+          <svg xmlns="http://www.w3.org/2000/svg" className="icon-half-human" fill="none" viewBox="0 0 24 24" stroke="currentColor">
+            <path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M12 12c2.761 0 5-2.239 5-5S14.761 2 12 2 7 4.239 7 7s2.239 5 5 5zm-7 8v-1a5 5 0 015-5h4a5 5 0 015 5v1a2 2 0 01-2 2H7a2 2 0 01-2-2z" />
+          </svg>
+
+          {/* 텍스트 콘텐츠 */}
+          {
+            user ? (
+              <span className={`${isDarkMode ? 'dark-mode' : ''}`}>{user.name}</span>
+            ) : (
+              <span className={`${isDarkMode ? 'dark-mode' : ''}`}>UbiSam</span>
+            )
+          } 
+    </label>
+    
+        <button className="NewlogOut-button" onClick={onLogout}>
+          {/* 로그인 아이콘 */}
+          {/* <svg xmlns="http://www.w3.org/2000/svg" className="icon-lock" fill="none" viewBox="0 0 24 24" stroke="currentColor">
+            <path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M16 11V7a4 4 0 00-8 0v4M5 11h14a2 2 0 012 2v7a2 2 0 01-2 2H5a2 2 0 01-2-2v-7a2 2 0 012-2z" />
+          </svg> */}
+          {/* 로그아웃 아이콘 */}
+          <svg xmlns="http://www.w3.org/2000/svg" className="icon-lock" fill="none" viewBox="0 0 24 24" stroke="currentColor">
+            <path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M17 16l4-4m0 0l-4-4m4 4H7m6 4v1a2 2 0 01-2 2H5a2 2 0 01-2-2V7a2 2 0 012-2h6a2 2 0 012 2v1" />
+          </svg>
+
+
+          {/* 로그인 텍스트 */}
+          <span className="NewlogOut-text">LOGOUT</span>
+
+          {/* 화살표 아이콘 */}
+          {/* <svg xmlns="http://www.w3.org/2000/svg" className="icon-arrow" fill="none" viewBox="0 0 24 24" stroke="currentColor">
+            <path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M9 5l7 7-7 7" />
+          </svg> */}
         </button>
       </div>
     </header>
